59733d09142ed3eeb0de33b32fa9308cc551e299
ghoney47 <gkkh2024@mymail.pomona.edu>
2025-12-21 17:24:49 -0800

    added theme config and some UI updating

--- app.py
+++ app.py
@@ -8,22 +8,50 @@ st.title("Graph Visualizer")
 
 st.markdown('### Create Graph')
 graph_type = st.selectbox("Graph Type", ("-","Directed", "Undirected", "DAG"))
-edge_type = st.radio("Edge Type", ["Weighted", "Unweighted"]) ##TODO: if unweighted, set edge upper/lower to 0
-spans = st.radio("Spanning", ["Yes", "No"])
-node_count = st.slider("Number of Nodes")
-edge_count = st.slider("Number of Edges")
-st.markdown('#### Edge Weight Range')
-edge_upper = st.slider("Upper", min_value=-100, max_value=100, value=0)
-edge_lower = st.slider("Lower", min_value=-100, max_value=100, value=0)
+
+##non-dag options
+if graph_type == "Directed" or graph_type == "Undirected":
+    edge_type = st.radio("Edge Type", ["Weighted", "Unweighted"]) ##TODO: if unweighted, set edge upper/lower to 0
+    spans = st.radio("Spanning", ["Yes", "No"])
+    node_count = st.slider("Number of Nodes")
+    edge_count = st.slider("Number of Edges")
+
+    ##sliders for weights
+    if edge_type == "Weighted":
+        st.markdown('#### Edge Weight Range')
+        edge_upper = st.slider("Upper", min_value=-100, max_value=100, value=0)
+        edge_lower = st.slider("Lower", min_value=-100, max_value=100, value=0)
 
 
-st.markdown('### Failure Graph')
-st.selectbox("Failure Graphs", ("ex1")) ##TODO: add failure graphs for selection
+    st.markdown('### Failure Graph')
+    st.selectbox("Failure Graphs", ("ex1")) ##TODO: add failure graphs for selection
+
+    ##TODO: create graphs within this scope
+
+##DAG selection
+else:
+    edge_type = st.radio("Edge Type", ["Weighted", "Unweighted"]) ##TODO: if unweighted, set edge upper/lower to 0
+    node_count = st.slider("Number of Nodes")
+    edge_count = st.slider("Number of Edges")
+
+    ##only allows source selection with valid nodes
+    if node_count > 0:
+        sources = st.slider("Sources", min_value=0, max_value=node_count)
+
+    ##sliders for weights
+    if edge_type == "Weighted":
+        st.markdown('#### Edge Weight Range')
+        edge_upper = st.slider("Upper", min_value=-100, max_value=100, value=0)
+        edge_lower = st.slider("Lower", min_value=-100, max_value=100, value=0)
+
+    ##TODO: create DAGs within this scope
 
 st.sidebar.text("Click here to Generate!")
+
+##if unweighted, set weights to 0
 with st.sidebar.container(horizontal=False, vertical_alignment="distribute"):
     if st.button("Generate Custom", type="primary", shortcut="G"):
-        ##TODO: generate graph
+        ##TODO: generate graph 
         print("user selected user graph")
     if st.button("Generate Random", type="primary", shortcut="Shift+G"):
         ##TODO: generate graph
